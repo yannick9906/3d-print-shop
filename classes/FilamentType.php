98f3fd1c9cfc97db59bed716bf6b5f12bc124908
Yannick Felix <yannick.felix1999@gmail.com>
2016-04-27 21:42:13 +0200

    - Class: Filamenttype

--- classes/FilamentType.php
+++ classes/FilamentType.php
@@ -14,23 +14,114 @@
         private $colorname, $colorcode;
         private $price;
         private $saleprice;
+        private $available;
 
         /**
          * FilamentType constructor.
          *
-         * @param $diameter
-         * @param $colorname
-         * @param $colorcode
-         * @param $price
-         * @param $saleprice
+         * @param int $diameter
+         * @param string $colorname
+         * @param string  $colorcode
+         * @param int $price
+         * @param int $saleprice
+         * @param int $available
          */
-        public function __construct($diameter, $colorname, $colorcode, $price, $saleprice) {
+        public function __construct($diameter, $colorname, $colorcode, $price, $saleprice, $available) {
             $this->diameter = $diameter;
             $this->colorname = $colorname;
             $this->colorcode = $colorcode;
             $this->price = $price;
             $this->saleprice = $saleprice;
+            $this->available = $available == 1;
         }
 
+        public static function fromFID($fID) {
+            $pdo = new PDO_MYSQL();
+            $res = $pdo->query("SELECT * FROM print3d_filamenttypes WHERE fID = :fid", [":fid" => $fID]);
+            return new FilamentType($res->diameter, $res->colorname, $res->colorcode, $res->price, $res->salesprice, $res->available)
+        }
 
+        /**
+         * @return mixed
+         */
+        public function getDiameter() {
+            return $this->diameter;
+        }
+
+        /**
+         * @param mixed $diameter
+         */
+        public function setDiameter($diameter) {
+            $this->diameter = $diameter;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getColorname() {
+            return $this->colorname;
+        }
+
+        /**
+         * @param mixed $colorname
+         */
+        public function setColorname($colorname) {
+            $this->colorname = $colorname;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getColorcode() {
+            return $this->colorcode;
+        }
+
+        /**
+         * @param mixed $colorcode
+         */
+        public function setColorcode($colorcode) {
+            $this->colorcode = $colorcode;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getPrice() {
+            return $this->price;
+        }
+
+        /**
+         * @param mixed $price
+         */
+        public function setPrice($price) {
+            $this->price = $price;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getSaleprice() {
+            return $this->saleprice;
+        }
+
+        /**
+         * @param mixed $saleprice
+         */
+        public function setSaleprice($saleprice) {
+            $this->saleprice = $saleprice;
+        }
+
+        /**
+         * @param bool $available
+         */
+        public function setAvailable($available) {
+            $this->available = $available;
+        }
+
+        /**
+         * @return bool
+         */
+        public function isAvailable() {
+            return $this->available;
+        }
     }
