85497ea09b8b558fe9c9050f00c370c9bf82fb13
Yannick Felix <yannick.felix1999@gmail.com>
2016-04-28 21:12:00 +0200

    Backend Validate Username
    Frontend Register

--- js/login.js
+++ js/login.js
@@ -21,5 +21,56 @@ function login() {
 }
 
 function register() {
+    var usrname  = $("#usrname").val();
+    var passwd1  = $("#passwd").val();
+    var passwd2  = $("#passwd2").val();
+    var email    = $("#email").val();
+    var realname = $("#realname").val();
 
+    if(passwd1 == passwd2) {
+        var passwd = md5(passwd1);
+        data = {
+            usrname: usrname,
+            passwd: passwd,
+            email: email,
+            realname: realname
+        };
+        $.post("users.php?action=register", data, function(data) {
+            data = JSON.parse(data);
+            var field = $("#usrname");
+            if(data["success"] == 1) {
+                Materialize.toast('Registrierung erfolgreich', 2000, 'green');
+                switchToLogin();
+                field.removeClass("invalid");
+                field.removeClass("valid");
+            } else if(data["errorcode"] == 1) {
+                Materialize.toast('Der Benutzername ist schon vergeben', 2000, 'red');
+                field.addClass("invalid");
+                field.removeClass("valid");
+            } else if(data["errorcode"] == 2) {
+                Materialize.toast('Es müssen alle Felder ausgefüllt sein', 2000, 'red');
+            } else {
+                Materialize.toast('Es ist ein Fehler aufgetreten. Das tut uns leid :/', 2000, 'red');
+            }
+
+        });
+    } else {
+        Materialize.toast('Passwörter stimmen nicht überein', 2000, 'red');
+    }
+}
+
+function checkUsrname() {
+    var field = $("#usrname");
+    $.getJSON("users.php?action=validateUsername&username="+field.val(), null, function(data) {
+        if(field.val() == "") {
+            field.removeClass("invalid");
+            field.removeClass("valid");
+        } else if(data["success"] == 1) {
+            field.removeClass("invalid");
+            field.addClass("valid");
+        } else {
+            field.addClass("invalid");
+            field.removeClass("valid");
+        }
+    });
 }
--- users.php
+++ users.php
@@ -5,6 +5,7 @@
      * Date: 28.04.2016
      * Time: 17:12
      */
+    error_reporting(E_ALL);
 
     require_once "classes/PDO_MYSQL.php";
     require_once "classes/User.php";
@@ -14,7 +15,7 @@
     if($action == "login") {
 
     } elseif($action == "register") {
-        $jsonarray = [];
+        $jsonarray = ["success" => 0];
         $username  = $_POST["usrname"];
         $realname  = $_POST["realname"];
         $passwd    = $_POST["passwd"];
@@ -24,9 +25,13 @@
             $jsonarray["errorcode"] = 1;
             $jsonarray["errormsg"] = "Der Benutzername ist bereits vergeben";
         } elseif($username != null and $realname != null and $passwd != null and $email != null) {
-            \print3d\User::createUser($usrname, $email, $passwd, $realname);
+            \print3d\User::createUser($username, $email, $passwd, $realname);
+            $jsonarray["success"] = 1;
         } else {
             $jsonarray["errorcode"] = 2;
             $jsonarray["errormsg"] = "Das Formular muss komplett ausgefüllt sein";
         }
+        echo json_encode($jsonarray);
+    } elseif($action == "validateUsername") {
+        echo json_encode(["success" => \print3d\User::doesUserNameExist($_GET["username"]) ? 0:1]);
     }
