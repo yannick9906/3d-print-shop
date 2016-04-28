8d128bd784da186d690c84c5d9d9f43a5dc847f7
Yannick Felix <yannick.felix1999@gmail.com>
2016-04-28 17:13:30 +0200

    Javascript for Login / Register Design

---
+++ js/login.js
@@ -0,0 +1,25 @@
+/**
+ * Created by yanni on 28.04.2016.
+ */
+
+function switchToRegister() {
+    $("#btnToLogin").show();
+    $("#login").hide();
+    $("#btnToRegister").hide();
+    $("#register").show();
+}
+
+function switchToLogin() {
+    $("#btnToLogin").hide();
+    $("#login").show();
+    $("#btnToRegister").show();
+    $("#register").hide();
+}
+
+function login() {
+
+}
+
+function register() {
+
+}
---
+++ users.php
@@ -0,0 +1,9 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: yanni
+     * Date: 28.04.2016
+     * Time: 17:12
+     */
+
+$action = $_GET["action"];
