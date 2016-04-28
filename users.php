874aa2dbbd208f4a076a16d5ff4aa4e431deb7c1
Yannick Felix <yannick.felix1999@gmail.com>
2016-04-28 17:19:46 +0200

    Backend Register new User

--- users.php
+++ users.php
@@ -6,4 +6,27 @@
      * Time: 17:12
      */
 
-$action = $_GET["action"];
+    require_once "classes/PDO_MYSQL.php";
+    require_once "classes/User.php";
+
+    $action = $_GET["action"];
+
+    if($action == "login") {
+
+    } elseif($action == "register") {
+        $jsonarray = [];
+        $username  = $_POST["usrname"];
+        $realname  = $_POST["realname"];
+        $passwd    = $_POST["passwd"];
+        $email     = $_POST["email"];
+
+        if(\print3d\User::doesUserNameExist($username)) {
+            $jsonarray["errorcode"] = 1;
+            $jsonarray["errormsg"] = "Der Benutzername ist bereits vergeben";
+        } elseif($username != null and $realname != null and $passwd != null and $email != null) {
+            \print3d\User::createUser($usrname, $email, $passwd, $realname);
+        } else {
+            $jsonarray["errorcode"] = 2;
+            $jsonarray["errormsg"] = "Das Formular muss komplett ausgefüllt sein";
+        }
+    }
