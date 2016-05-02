8fa6778384577c037e9324f0dba35b1879cdef6b
Yannick Felix <yannick.felix1999@gmail.com>
2016-05-02 22:49:37 +0200

    Thing Detail finished Added Buttons for Actions

--- js/appUser.js
+++ js/appUser.js
@@ -117,14 +117,16 @@ function showDetail(oid) {
             $("#detail-precision")     .html(data["order"]["precision"]);
             $("#detail-color")         .html(data["order"]["filamentcolorname"]);
             $("#detail-weight")        .html(data["order"]["material_weight"]);
+            $("#detail-printtime")     .html(data["order"]["printtime"]);
             $("#detail-date-created")  .html(data["order"]["date_created"]);
             $("#detail-date-confirmed").html(data["order"]["date_confirmed"]);
             $("#detail-date-completed").html(data["order"]["date_completed"]);
             $("#detail-material-cost") .html(data["order"]["material_price"]);
             $("#detail-energy-cost")   .html(data["order"]["energy_price"]);
             $("#detail-length")        .html(data["order"]["material_length"]);
-            $("#detail-per-kg")        .html(data["order"][""]);
+            $("#detail-per-kg")        .html(data["order"]["filamentprice"]);
             $("#detail-total-cost")    .html(data["order"]["complete_price"]);
+            setButtonsForState(parseInt(data["order"]["state"]));
             var link = data["order"]["order_link"];
             if(link.contains("thingiverse")) {
                 detail_link = $("#detail_link");
@@ -227,4 +229,51 @@ function updateEmail() {
                 Materialize.toast('Es ist ein Fehler aufgetreten. Das tut uns leid :/', 2000, 'red');
             }
         });
+}
+
+function setButtonsForState(state) {
+    console.log(state);
+    switch(state) {
+        case -2:
+            setButton(1, 0, 1, 0, 0);
+            break;
+        case -1:
+            setButton(1, 0, 1, 0, 0);
+            break;
+        case 0:
+            setButton(1, 0, 0, 0, 0);
+            break;
+        case 1:
+            setButton(1, 0, 1, 0, 0);
+            break;
+        case 2:
+            setButton(0, 0, 1, 0, 0);
+            break;
+        case 3:
+            setButton(0, 0, 1, 0, 0);
+            break;
+        case 4:
+            setButton(0, 0, 1, 0, 0);
+            break;
+        case 5:
+            setButton(0, 0, 1, 1, 1);
+            break;
+        case 6:
+            setButton(1, 0, 1, 0, 1);
+            break;
+        case 7:
+            setButton(0, 0, 1, 0, 1);
+            break;
+        case 8:
+            setButton(0, 0, 1, 0, 0);
+            break;
+    }
+}
+
+function setButton(deleteBtn, order, reorder, arrived, warranty) {
+    if(deleteBtn == 1) $("#btn-delete").show();   else $("#btn-delete").hide();
+    if(order == 1)     $("#btn-order").show();    else $("#btn-order").hide();
+    if(reorder == 1)   $("#btn-reorder").show();  else $("#btn-reorder").hide();
+    if(arrived == 1)   $("#btn-arrived").show();  else $("#btn-arrived").hide();
+    if(warranty == 1)  $("#btn-warranty").show(); else $("#btn-warranty").hide();
 }
