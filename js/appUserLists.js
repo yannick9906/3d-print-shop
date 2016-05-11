437213a88116603051a18fe2c3bfd2431d1f0202
Yannick Felix <yannick.felix1999@gmail.com>
2016-05-11 23:10:36 +0200

    Progress indicator for User Mode

--- js/appUserLists.js
+++ js/appUserLists.js
@@ -7,6 +7,7 @@ function update() {
         $.getJSON("orders.php?action=getOwnOrders",null,function(data) {
             if(data["error"] == "NoLogin") window.location.href = "appLogin.html";
             else if(!(JSON.stringify(oldData) == JSON.stringify(data))) {
+                $("#loading").fadeIn(100);
                 $("#orders").html("");
                 data["orders"].forEach(function (element, index, array) {
                     if(data["error"] == "NoLogin") window.location.href = "appLogin.html";
@@ -17,11 +18,13 @@ function update() {
                 });
                 oldData = data;
             }
+            $("#loading").fadeOut(100);
         });
     } else if(mode == "OldOrders") {
         $.getJSON("orders.php?action=getOwnOldOrders",null,function(data) {
             if(data["error"] == "NoLogin") window.location.href = "appLogin.html";
             else if(!(JSON.stringify(oldData) == JSON.stringify(data))) {
+                $("#loading").fadeIn(100);
                 $("#orders").html("");
                 data["orders"].forEach(function (element, index, array) {
                     if(data["error"] == "NoLogin") window.location.href = "appLogin.html";
@@ -32,6 +35,7 @@ function update() {
                 });
                 oldData = data;
             }
+            $("#loading").fadeOut(100);
         });
     }
 }
